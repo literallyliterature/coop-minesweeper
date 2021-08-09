3ec3f05ee1508ee744557274667454c5ed2886bc
Sahitya Beri <sahityaberi@gmail.com>
2021-08-09 21:51:16 +1000

    Revert Fastclick

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Fastclick from 'fastclick';
 import { longClickDirective } from 'vue-long-click';
 
 import App from './App.vue';
@@ -10,6 +11,8 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
+
+document.onload = () => Fastclick(document.body);
 
 const longClickInstance = longClickDirective({ delay: 300, interval: 150000 });
 Vue.directive('long-click', longClickInstance);
